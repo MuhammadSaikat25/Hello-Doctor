1d4b705c46d8093bfa09c9047a7ee346feb2459e
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-07-04 09:02:37 +0600

    added Appointment conform in mongodb

--- src/Component/FireBase/Appoinment/BookAppointment.jsx
+++ src/Component/FireBase/Appoinment/BookAppointment.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../AuthProvider";
-import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const BookAppointment = ({ data }) => {
     const { user } = useContext(AuthContext)
@@ -8,12 +9,32 @@ const BookAppointment = ({ data }) => {
 
     const Booking = () => {
 
-        
-
+        const appointment = {
+            email: user?.email,
+            doctorName: data.name,
+            doctorImage: data.image,
+            price: data.price,
+            services: data.services
+        }
+        fetch(`${import.meta.env.VITE_SERVER}Patients`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(appointment)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    toast("Appointment conform!")
+                }
+            })
     }
 
     return (
         <div className='border w-fit p-1 space-y-2 mx-auto' key={data._id}>
+
+            <ToastContainer />
             <img className='w-[300px] mx-auto h-[300px] object-contain' src={data.image} alt="" />
             <h1 className='font-bold'>{data.name}</h1>
             <h1 className='font-bold'>Education: {data.education}</h1>
